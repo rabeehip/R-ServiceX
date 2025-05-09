7ba3e2c214b89b485d143e7254b74c7ed20f7d14
rabeehip <140885024+rabeehip@users.noreply.github.com>
2025-05-09 18:55:52 +0530

    update app.js

--- app.js
+++ app.js
@@ -6380,7 +6380,7 @@ function file_video(path) {
 	<br>${playBtn}
 	<!-- ???? -->
     <div class="mdui-textfield">
-      <label class="mdui-textfield-label">╰┈➤ Name</label>
+      <label class="mdui-textfield-label">╰┈➤File name</label>
 	  <input class="mdui-textfield-input" type="text" value="${file_name}"/>
     </div>
 	<div class="mdui-textfield">
