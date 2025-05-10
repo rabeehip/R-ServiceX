3c27e0d4c2c9439ebf1decb85fa9133e620b1705
rabeehip <140885024+rabeehip@users.noreply.github.com>
2025-05-10 06:34:42 +0530

    update app.js

--- app.js
+++ app.js
@@ -6190,7 +6190,7 @@ function onSearchResultItemClick(a_ele) {
       mdui.dialog({
         title: `
            <div class="mdui-text-center mdui-typo-title mdui-m-b-1">
-           Choose your opted-for option <i class="mdui-icon material-icons">info</i>
+           Choose your opted-for method from below<i class="mdui-icon material-icons">info</i>
            </div>
         `,
         content: linkHTML,
